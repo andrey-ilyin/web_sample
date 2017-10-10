1011751947ea8fa73a9bc7b1731b3fd5304c834a
Andrey Ilyin <andrey.ilyin@ispringsolutions.com>
2017-10-10 20:47:26 +0300

    Добавлен пример использования шаблонизатора Twig.

---
+++ index.php
@@ -0,0 +1,8 @@
+﻿<?php
+require_once("include/common.inc.php");
+
+$vars = array(
+    'name' => 'Stephen King', 
+    'books' => array(
+        'Pet Sematary', 'Christine', 'Needful Things'));
+echo getView('index.twig', $vars);
